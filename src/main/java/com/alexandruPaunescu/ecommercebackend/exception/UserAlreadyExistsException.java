5f2ee891a645bda7ae118acbc483ddd85570bff8
Alexandru Paunescu <paunscualexandru@gmail.com>
2022-12-06 23:31:08 +0200

    First commit, back-end for e-commerce website, using Spring, Spring Boot and Hibernate.

---
+++ src/main/java/com/alexandruPaunescu/ecommercebackend/exception/UserAlreadyExistsException.java
@@ -0,0 +1,4 @@
+package com.alexandruPaunescu.ecommercebackend.exception;
+
+public class UserAlreadyExistsException  extends Exception{
+}
---
+++ src/main/java/com/alexandruPaunescu/ecommercebackend/model/dao/LocalUserDAO.java
@@ -0,0 +1,15 @@
+package com.alexandruPaunescu.ecommercebackend.model.dao;
+
+import com.alexandruPaunescu.ecommercebackend.model.LocalUser;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface LocalUserDAO extends CrudRepository<LocalUser,Long> {
+    Optional<LocalUser> findByUsernameIgnoreCase(String username);
+
+    Optional<LocalUser> findByEmailIgnoreCase(String email);
+
+
+
+}
